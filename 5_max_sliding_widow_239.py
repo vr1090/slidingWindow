5369926769f050fb0f38d000e8df4b872e7899f6
inu <vr1090@gmail.com>
2022-02-17 06:42:03 +0700

    max sliding window

---
+++ 5_max_sliding_widow_239.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        l = r = 0
+        q = collections.deque()
+        result = []
+        
+        while r < len(nums):
+            while q and nums[r] > nums[q[-1]]:
+                q.pop()
+            
+            q.append(r)
+            
+            if l > q[0]:
+                q.popleft()
+            
+            if (r-l +1 ==k):
+                result.append(nums[q[0]])
+                l = l + 1
+            
+            r = r + 1
+        
+        return result
