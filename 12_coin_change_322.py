90e17fdbd84ccd621151e933a823faeac02a8b4b
inu <vr1090@gmail.com>
2022-02-22 11:59:30 +0700

    add coin change

---
+++ 12_coin_change_322.py
@@ -0,0 +1,13 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [0] * (amount+1)
+        
+        for i in range(1,amount+1):
+            minimum = 100000
+            for c in coins:
+                if( i-c >=0):
+                    minimum = min(minimum, 1+dp[i-c])
+            
+            dp[i] = minimum
+        
+        return dp[amount] if dp[amount] < 100000 else -1
