4e8630c37ee612f94c99a65b3fb45522384be79f
inu <vr1090@gmail.com>
2022-02-26 23:55:37 +0700

    course schedule

---
+++ 21_course_schedule_207.py
@@ -0,0 +1,34 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+            dictDepend = {i:[] for i in range(numCourses)}
+            visited = set()
+            
+            
+            for lst in prerequisites:
+                dictDepend[lst[0]].append( lst[1])
+            
+            def dfs(num):
+                if num in visited:
+                    return False
+                
+                if dictDepend[num] == [] :
+                    return True
+                
+                visited.add(num)
+                
+                for n in dictDepend[num]:
+                    if not dfs(n): return False
+                
+                visited.remove(num)
+                dictDepend[num] = []
+                
+                return True
+                    
+                    
+                    
+            for n in range(numCourses):
+                if not dfs(n):
+                    return False
+            
+            return True
+                
